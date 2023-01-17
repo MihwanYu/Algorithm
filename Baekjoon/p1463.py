16fa75327698cda33e3d744df80b77be48b96792
MihwanYu <sally9804@gmail.com>
2023-01-18 02:06:12 +0900

    boj 1463

--- Baekjoon/p1463.py
+++ Baekjoon/p1463.py
@@ -1,25 +1,37 @@
-# DP 다시풀어야함
+# DP 다시풀어야함 -> 처음으로 혼자 성공한 DP,,,흑흑
 def solution():
     X = int(input())
     count = 0
 
-    dp = [[X,X,X]]
-    i = 0
-    while 1 not in dp[-1]:
-        ascend = sorted(dp[i])
-        dp0 = dp1 = dp2 = -1
-        for n in ascend[::-1]:
-            if n%3==0:
-                dp0 = n//3
-            if n%2==0:
-                dp1 = n//2
-            if n > 1:
-                dp2 = n-1
-        dp.append([dp0, dp1, dp2])
-        print(dp[-1])
-        i +=1
-        count += 1
+    dp = [-1]*(X+1)
+    
+    dp[X]=0
+    dp[X-1]=1
+    if X%2==0: dp[X//2]=1
+    if X%3==0: dp[X//3]=1
+
+    for i in range(X-1, 0, -1):
+        # if i==9: print('dp when i==9: ',dp)
+        if dp[i]==-1:
+            dp[i] = dp[i+1]+1
+        else:
+            dp[i] = min(dp[i], dp[i+1]+1)
+
+        if i%2==0:
+            if dp[i//2] != -1:
+                dp[i//2] = min(dp[i//2], dp[i]+1)
+            else:
+                dp[i//2] = dp[i]+1
+
+        if i%3==0:
+            if dp[i//3] != -1:
+                dp[i//3] = min(dp[i//3], dp[i]+1)
+            else:
+                dp[i//3] = dp[i]+1
+        # if i==9: print('dp when i==9: ',dp)
+    # print('dp: ',dp)
+
         
-    print('count: ',count)
+    print(dp[1])
 if __name__=="__main__":
     solution()
